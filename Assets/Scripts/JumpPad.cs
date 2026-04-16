bd4dd28a14fa048f77fc10f4ada28e189ea4565a
Mikkelmig <mikkelhholm@gmail.com>
2026-04-16 13:20:16 +0200

    Graphics & Sound
    - Death sound edit
    - BouncePad edit
    - Dash sound edit
    - BouncePad graphics edit
    - Level graphics setup

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -1,14 +1,18 @@
+using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class JumpPad : MonoBehaviour
 {
     [SerializeField] float jumpForce = 500f;
     public AudioClip jumpLyd;
     private AudioSource audioSource;
+    private Animator animator;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,9 +23,18 @@ public class JumpPad : MonoBehaviour
             {
                 rb.AddForce(new Vector2(0, jumpForce));
 
-                if (audioSource != null && jumpLyd != null)
+                //if (audioSource != null && jumpLyd != null)
+                //{
+                //    audioSource.PlayOneShot(jumpLyd);
+                //}
+
+
+                if (animator != null)   
                 {
-                    audioSource.PlayOneShot(jumpLyd);
+                    //animator.SetBool("isActiveted", true);
+                    //Debug.Log("jump");
+                    StopAllCoroutines();
+                    StartCoroutine(Animation(1f));
                 }
             }
         }
@@ -29,6 +42,24 @@ public class JumpPad : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        //if (animator != null)
+        //{
+        //    StartCoroutine(Delay(1f));
+        //    animator.SetBool("isActiveted", false);
+
+        //}
+
+    }
+
+    private IEnumerator Animation(float f)
+    {
+        animator.SetBool("isActiveted", true);
+        Debug.Log("jump");
+        if (audioSource != null && jumpLyd != null)
+        {
+            audioSource.PlayOneShot(jumpLyd);
+        }
+        yield return new WaitForSeconds(f);
+        animator.SetBool("isActiveted", false);
     }
 }
